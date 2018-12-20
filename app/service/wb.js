398416e9acb96ce808ec328ef5c6aca8335e959c
maixiaojie <778273234@qq.com>
2018-12-20 18:09:35 +0800

    微博统计接口

---
+++ app/service/wb.js
@@ -0,0 +1,24 @@
+'use strict';
+const Service = require('egg').Service;
+
+class WbService extends Service {
+    async getSum(uid) {
+        const sql = `SELECT SUM(reposts_count) AS rc,
+        SUM(comments_count) AS cc,
+        SUM(attitudes_count) AS ac
+        FROM wb WHERE uid = '${uid}'  AND LENGTH(created_at) != 10`
+        const rs = await this.app.mysql.query(sql);
+        return rs;
+    }
+    async getcount(uid) {
+        const sql = `SELECT COUNT(*) FROM wb WHERE uid = '${uid}' AND LENGTH(created_at) != 10`
+        const rs = await this.app.mysql.query(sql);
+        return rs;
+    }
+    async getRetweentedCount(uid) {
+        const sql = `SELECT COUNT(*) FROM wb WHERE uid = '${uid}' AND is_retweeted = '1' AND LENGTH(created_at) != 10`;
+        const rs = await this.app.mysql.query(sql);
+        return rs;
+    }
+}
+module.exports = WbService;
